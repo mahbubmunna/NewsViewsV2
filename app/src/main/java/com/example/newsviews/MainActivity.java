a02496fba2725c4d64464fe3bd88cbb448fd2d32
Mahbub Munna <mahbubhassanmunna@gmail.com>
2019-02-16 22:48:55 +0600

    Secondary Splash cold time implementation

--- app/src/main/java/com/example/newsviews/MainActivity.java
+++ app/src/main/java/com/example/newsviews/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Setting the splash theme before onCreate of parent class
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
